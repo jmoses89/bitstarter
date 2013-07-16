aed7f9ff54e2786806d044ef6dfcc81b964e61fd
Jarret Moses <jarret.moses@gmail.com>
2013-07-16 15:45:48 +0000

    Added Buffer Read

--- web.js
+++ web.js
@@ -2,9 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buf = fs.readFileSync('index.html');
+var binbuf = buf.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send( binbuf );
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
